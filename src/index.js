19824dd07f5743d2f8f8dccf1756d3dabeec94f6
Henrique Pizzol Grando <hp2409@columbia.edu>
2017-01-26 18:01:30 -0200

    Implements subscription handler

--- src/index.js
+++ src/index.js
@@ -4,5 +4,25 @@ var stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = function(event, context) {
 
-  
+    stripe.customers.create({
+      email: event.email,
+      source: event.cc,
+    }, function(err, customer) {
+      if(err){
+        context.fail(err);
+      }
+      else {
+        stripe.subscriptions.create({
+          customer: customer.id,
+          plan: event.plan,
+        }, function(err, subscription) {
+          if(err){
+            context.fail(err);
+          }
+          else{
+            context.succeed({ customer: customer.id, success : true });
+          }
+        });
+      }
+    });
 };
